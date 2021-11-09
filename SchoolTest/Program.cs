5c6cf7f134b232ef2b9dbf0ac2a88f1d981f13c9
Shalom William <shalomwill98@gmail.com>
2021-11-09 16:55:17 +0100

    initial commit

---
+++ SchoolTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SchoolTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
